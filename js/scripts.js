ddbc1587ec98be7d8c12aac3f65ab1b44ff678a9
Austin Schrader <austin.jschrader@gmail.com>
2018-10-18 14:56:41 -0700

    Make the animal website using jQuery

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $(".showdog").click(function() {
+    $("#dog-info").toggle();
+    $("#dog-hidden").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".showcat").click(function() {
+    $("#cat-info").toggle();
+    $("#cat-hidden").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".showfish").click(function() {
+    $("#fish-info").toggle();
+    $("#fish-hidden").toggle();
+  });
+});
